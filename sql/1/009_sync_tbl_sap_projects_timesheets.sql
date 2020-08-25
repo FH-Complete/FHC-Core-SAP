a7303b5dde0b539fac4e75b5800ea0ee574af9e3
Paolo <bison@technikum-wien.at>
2020-08-25 17:16:06 +0200

    - Added new numeric column status to table sync.tbl_sap_projects_timesheets - Added new date column update date to table sync.tbl_sap_projects_timesheets - Added comments to start and end date columns of table sync.tbl_sap_projects_timesheets - Made columns project_object_id and project_task_object_id of table sync.tbl_sap_projects_timesheets unique together

--- sql/1/009_sync_tbl_sap_projects_timesheets.sql
+++ sql/1/009_sync_tbl_sap_projects_timesheets.sql
@@ -5,7 +5,9 @@ CREATE TABLE IF NOT EXISTS sync.tbl_sap_projects_timesheets (
 	project_task_id character varying(42),
 	project_task_object_id character varying(42),
 	start_date timestamp DEFAULT NULL,
-	end_date timestamp DEFAULT NULL
+	end_date timestamp DEFAULT NULL,
+	status numeric NOT NULL,
+	updateamum timestamp without time zone DEFAULT NULL
 );
 
 COMMENT ON TABLE sync.tbl_sap_projects_timesheets IS 'Table to save SAP ByD Poject IDs';
@@ -13,6 +15,10 @@ COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.project_id IS 'SAP Project ID
 COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.project_object_id IS 'SAP Project Object ID ';
 COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.project_task_id IS 'SAP Project Task ID';
 COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.project_task_object_id IS 'SAP Project Task Object ID ';
+COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.start_date IS 'SAP Project/Task start date';
+COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.end_date IS 'SAP Project/Task end date';
+COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.status IS 'SAP Project/Task status (3 => released)';
+COMMENT ON COLUMN sync.tbl_sap_projects_timesheets.updateamum IS 'Last record update timestamp, if null then never updated';
 
 CREATE SEQUENCE IF NOT EXISTS sync.tbl_sap_projects_timesheets_projects_timesheet_id_seq
     INCREMENT BY 1
@@ -34,6 +40,13 @@ DO $$
 BEGIN
 	ALTER TABLE sync.tbl_sap_projects_timesheets ADD CONSTRAINT uk_sap_projects_timesheets
 	UNIQUE (project_id, project_object_id, project_task_id, project_task_object_id);
+	EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
+
+DO $$
+BEGIN
+	ALTER TABLE sync.tbl_sap_projects_timesheets ADD CONSTRAINT uk_sap_projects_timesheets_2
+	UNIQUE (project_object_id, project_task_object_id);
 	EXCEPTION WHEN OTHERS THEN NULL;
 END $$;
 
