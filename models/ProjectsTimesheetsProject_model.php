275779f48febcde86316cb4adab3b1b1b9ad5100
Cris <hainberg@technikum-wien.at>
2020-09-30 13:45:00 +0200

    Created methods to synchronize SAP and FH projects / phases
    . Get SAP phases of a given SAP project and if they are synchronized
    with FUE phases.
    . Get FUE phases of a given FUE project and if they are synchronized
    with SAP phases.
    . Insert SAP and FH project into the sync-table.
    . Check, if SAP project is synchronized.
    . Check, if SAP projectphase is synchronized.
    . Check, if FUE project is synchronized.
    . Check, if FUE projectphase is synchronized.

--- models/ProjectsTimesheetsProject_model.php
+++ models/ProjectsTimesheetsProject_model.php
@@ -9,7 +9,173 @@ class ProjectsTimesheetsProject_model extends DB_Model
 	{
 		parent::__construct();
 		$this->dbTable = 'sync.tbl_projects_timesheets_project';
-		$this->pk = array('projects_timesheets_project_id')
+		$this->pk = array('projects_timesheets_project_id');
+	}
+
+	/**
+	 * Get SAP phases of a given SAP project and if they are synchronized with FUE phases.
+	 * @param string $project_id
+	 * @return mixed
+	 */
+	public function getSAPPhases_withSyncStatus($project_id)
+	{
+		$qry = '
+			WITH
+				sap_projectphases AS
+				(
+					SELECT  *
+					FROM    sync.tbl_sap_projects_timesheets
+					WHERE   project_id = ?
+	                AND     project_task_id IS NOT NULL
+				)
+				
+			SELECT
+	            CASE
+					WHEN projects_timesheets_project IS NOT NULL THEN \'true\'
+					ELSE \'false\'
+		        END AS "isSynced",
+	            projects_timesheets_project,
+	            projects_timesheet_id,
+	            project_id,
+	            project_task_id                
+			FROM        sap_projectphases 
+	        LEFT JOIN   sync.tbl_projects_timesheets_project USING (projects_timesheet_id)
+	        ORDER BY    projects_timesheets_project, project_task_id;
+		';
+
+		return $this->execQuery($qry, array($project_id));
+	}
+
+	/**
+	 * 	Get FUE phases of a given FUE project and if they are synchronized with SAP phases.
+	 * @param string $projekt_kurzbz
+	 * @return mixed
+	 */
+	public function getFUEPhases_withSyncStatus($projekt_kurzbz)
+	{
+		$qry = '
+			WITH
+				fue_projectphases AS
+				(
+					SELECT  *
+					FROM    fue.tbl_projekt
+					JOIN    fue.tbl_projektphase USING (projekt_kurzbz)
+					WHERE   projekt_kurzbz = ?
+				)
+				
+			SELECT
+	            CASE
+					WHEN projects_timesheets_project IS NOT NULL THEN \'true\'
+					ELSE \'false\'
+		        END AS "isSynced",
+	            projects_timesheets_project,
+	            fue_projectphases.projekt_id,
+	            projekt_kurzbz,
+	            projektphase_id,
+	            bezeichnung                   
+			FROM        fue_projectphases 
+            LEFT JOIN   sync.tbl_projects_timesheets_project USING (projektphase_id)
+            ORDER BY    projects_timesheets_project, bezeichnung;
+		';
+
+		return $this->execQuery($qry, array($projekt_kurzbz));
+	}
+
+	/**
+	 * Insert SAP and FH project into the sync-table.
+	 * @param int $projects_timesheet_id
+	 * @param int $projekt_id
+	 * @param int $projektphase_id
+	 * @return mixed
+	 */
+	public function syncProjectphases($projects_timesheet_id, $projekt_id, $projektphase_id)
+	{
+		return $this->insert(array(
+				'projects_timesheet_id' => $projects_timesheet_id,
+				'projekt_id' => $projekt_id,
+				'projektphase_id' => $projektphase_id
+			)
+		);
+	}
+
+	/**
+	 * Check, if SAP project is synchronized.
+	 * @param int $projects_timesheet_id of the SAP project
+	 * @return boolean
+	 */
+	public function isSynced_SAPProject($projects_timesheet_id)
+	{
+		if (!is_numeric($projects_timesheet_id))
+		{
+			return error('projects_timesheet_id muss eine Nummer sein.');
+		}
+
+		$result = $this->loadWhere(array(
+			'projects_timesheet_id' => $projects_timesheet_id,
+			'projektphase_id' => NULL
+		));
+
+		return hasData($result);
+	}
+
+	/**
+	 * Check, if SAP projectphase is synchronized.
+	 * @param int $projects_timesheet_id of the SAP projectphase
+	 * @return boolean
+	 */
+	public function isSynced_SAPProjectphase($projects_timesheet_id)
+	{
+		if (!is_numeric($projects_timesheet_id))
+		{
+			return error('projects_timesheet_id muss eine Nummer sein.');
+		}
+
+		$result = $this->loadWhere('
+			projects_timesheet_id = '. $projects_timesheet_id. ' AND
+			projektphase_id IS NOT NULL
+		');
+
+		return hasData($result);
+	}
+
+	/**
+	 * Check, if FUE project is synchronized.
+	 * @param int $projekt_id
+	 * @return boolean
+	 */
+	public function isSynced_FUEProject($projekt_id)
+	{
+		if (!is_numeric($projekt_id))
+		{
+			return error('projekt_id muss eine Nummer sein.');
+		}
+
+		$result = $this->loadWhere(array(
+			'projekt_id' => $projekt_id,
+			'projektphase_id' => NULL
+		));
+
+		return hasData($result);
+
+	}
+
+	/**
+	 * Check, if FUE projectphase is synchronized.
+	 * @param int $projektphase_id
+	 * @return boolean
+	 */
+	public function isSynced_FUEProjectphase($projektphase_id)
+	{
+		if (!is_numeric($projektphase_id))
+		{
+			return error('projektphase_id muss eine Nummer sein.');
+		}
+
+		$result = $this->loadWhere(array(
+			'projektphase_id' => $projektphase_id
+		));
+
+		return hasData($result);
 	}
 }
 
