875af962798205a7bac7866dbba0c514e8e80fdf
Paolo <bison@technikum-wien.at>
2021-05-05 00:41:46 +0200

    - Better shaped SyncPaymentsLib->createGutschrift - Better log and errors management in ManagePayments->createGutschrift

--- controllers/jobs/ManagePayments.php
+++ controllers/jobs/ManagePayments.php
@@ -65,53 +65,52 @@ class ManagePayments extends JQW_Controller
 	 */
 	public function createGutschrift()
 	{
-		$jobType = 'SAPPaymentGutschrift';
 		$this->logInfo('Start data synchronization with SAP ByD: Gutschrift');
 
 		// Gets the latest jobs
-		$lastJobs = $this->getLastJobs($jobType);
+		$lastJobs = $this->getLastJobs(SyncPaymentsLib::SAP_PAYMENT_GUTSCHRIFT);
 		if (isError($lastJobs))
 		{
-			$this->logError('An error occurred while creating payments in SAP', getError($lastJobs));
+			$this->logError(getCode($lastJobs).': '.getError($lastJobs), SyncPaymentsLib::SAP_PAYMENT_GUTSCHRIFT);
 		}
 		else
 		{
-			$person_arr = $this->_getPersonIdArray(getData($lastJobs));
-			$syncResult = $this->syncpaymentslib->createGutschrift($person_arr);
+			// Gets all the jobs in the queue to create credit memo
+			$syncResult = $this->syncpaymentslib->createGutschrift(mergeUsersPersonIdArray(getData($lastJobs)));
 
+			// Log the result
 			if (isError($syncResult))
 			{
-				$this->logError('An error occurred while creating payments in SAP', getError($syncResult));
+				// Save all the errors
+				$errors = getError($syncResult);
+
+				// If it is NOT an array...
+				if (isEmptyArray($errors))
+				{
+					// ...then convert it to an array
+					$errors = array($errors);
+				}
+				// otherwise it is already an array
+
+				// For each error found
+				foreach ($errors as $error)
+				{
+					$this->logError(getCode($syncResult).': '.$error);
+				}
 			}
 			else
 			{
-				// If non blocking errors are present...
-				if (hasData($syncResult))
-				{
-					if (!isEmptyArray(getData($syncResult)))
-					{
-						// ...then log them all as warnings
-						foreach (getData($syncResult) as $nonBlockingError)
-						{
-							$this->logWarning($nonBlockingError);
-						}
-					}
-					// Else if it a single message log it as info
-					elseif (!isEmptyString(getData($syncResult)))
-					{
-						$this->logInfo(getData($syncResult));
-					}
-				}
-
-				// Update jobs properties values
-				updateJobs(
-					getData($lastJobs), // Jobs to be updated
-					array(JobsQueueLib::PROPERTY_STATUS, JobsQueueLib::PROPERTY_END_TIME), // Job properties to be updated
-					array(JobsQueueLib::STATUS_DONE, date("Y-m-d H:i:s")) // Job properties new values
-				);
-
-				if (hasData($lastJobs)) $this->updateJobsQueue($jobType, getData($lastJobs));
+				$this->logInfo(getData($syncResult));
 			}
+
+			// Update jobs properties values
+			$this->updateJobs(
+				getData($lastJobs), // Jobs to be updated
+				array(JobsQueueLib::PROPERTY_STATUS, JobsQueueLib::PROPERTY_END_TIME), // Job properties to be updated
+				array(JobsQueueLib::STATUS_DONE, date("Y-m-d H:i:s")) // Job properties new values
+			);
+
+			if (hasData($lastJobs)) $this->updateJobsQueue(SyncPaymentsLib::SAP_PAYMENT_GUTSCHRIFT, getData($lastJobs));
 		}
 
 		$this->logInfo('End data synchronization with SAP ByD: Gutschrift');
